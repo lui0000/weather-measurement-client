a437c4533e9f6492a7224096409cb515328365e8
Lisa <elbondarenko04@gmail.com>
2024-10-22 21:29:57 +0300

    все

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.example.dto.MeasurementDTO;
 import org.example.dto.SensorDTO;
 import org.knowm.xchart.QuickChart;
@@ -8,6 +10,11 @@ import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -30,10 +37,6 @@ public class Client {
         String addUrl = "http://localhost:8080/measurement/add";
 
 
-
-
-
-
         switch (choice) {
             case 1:
                 sensorDTO.setName("sensor 1");
@@ -47,22 +50,12 @@ public class Client {
                 System.out.println(getResponse);
                 System.out.println(postResponse);
 
-//                getResponse.
-//
-//                double[] xData = new double[] { 0.0, 1.0, 2.0 };
-//                double[] yData = new double[] { 2.0, 1.0, 0.0 };
-//
-//                // Create Chart
-//                XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
-//
-//                // Show it
-//                new SwingWrapper(chart).displayChart();
-
+                doChangeGraph(getResponse);
 
                 break;
             case 2:
 
-                for(int i = 0; i < 101; i++) {
+                for (int i = 0; i < 101; i++) {
                     int randomNumber = random.nextInt(101) - 100;
                     int randomBoolean = random.nextInt(2);
                     if (randomBoolean == 0) {
@@ -81,15 +74,7 @@ public class Client {
                     System.out.println(getResponse1);
                     System.out.println(postResponse1);
 
-
-//                    double[] xData = new double[] { 0.0, 1.0, 2.0 };
-//                    double[] yData = new double[] { 2.0, 1.0, 0.0 };
-//
-//                    // Create Chart
-//                    XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
-//
-//                    // Show it
-//                    new SwingWrapper(chart).displayChart();
+                    doChangeGraph(getResponse1);
 
                 }
 
@@ -101,7 +86,33 @@ public class Client {
 
     }
 
+    public static void doChangeGraph(String jsonResponse) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
 
+            List<MeasurementDTO> weatherDataList = objectMapper.readValue(jsonResponse, new TypeReference<>() {});
+            List<Double> temperature = new ArrayList<>();
+            List<Long> timeData = new ArrayList<>();
+
+            for (MeasurementDTO data : weatherDataList) {
+                temperature.add(data.getTemperature());
+                long millis = Timestamp.valueOf(data.getCreatedAt()).getTime();
+                timeData.add(millis);
+            }
+
+            // Преобразуем данные в массивы для xChart
+            double[] xData = timeData.stream().mapToDouble(Long::doubleValue).toArray();
+            double[] yData = temperature.stream().mapToDouble(Double::doubleValue).toArray();
+
+            XYChart chart = QuickChart.getChart("Weather Data", "Time", "Temperature", "Temperature", xData, yData);
+
+            new SwingWrapper(chart).displayChart();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
